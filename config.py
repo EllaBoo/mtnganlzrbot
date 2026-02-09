4b2d22607d36cbb55ba93d9822e0e1978f84a6e1
Ella <boodooboycn@gmail.com>
2026-02-09 23:50:28 +0500

    fix: Dronor API URL default to localhost:4000

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     BOT_TOKEN: str = os.getenv("TELEGRAM_BOT_TOKEN", "")
-    DRONOR_API: str = os.getenv("DRONOR_API_URL", "http://localhost:9100")
+    DRONOR_API: str = os.getenv("DRONOR_API_URL", "http://localhost:4000")
     WEBAPP_URL: str = os.getenv("WEBAPP_URL", "")
     MAX_FILE_MB: int = int(os.getenv("MAX_FILE_SIZE_MB", "500"))
     MAX_HOURS: int = int(os.getenv("MAX_DURATION_HOURS", "4"))
